713b111378d57d8e5ef25a4ad2c1965eb5c820f9
João Victor <joaovictoralves087@gmail.com>
2026-04-05 10:24:43 -0300

    FIRST COMMIT

---
+++ src/test/java/cp02/cp02/Cp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package cp02.cp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
